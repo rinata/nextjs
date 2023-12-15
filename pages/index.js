88e3212eabba4df95994f2793ad1e5067226e005
Rizal Nasrulloh <rizalxrpl2@gmail.com>
2023-12-15 10:21:22 +0800

    #tugas 4 - install tailwind

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <h1 className={styles.center}>
+      <h1 className="text-3xl font-bold underline">
         Ini isi konten
       </h1>
     </>
