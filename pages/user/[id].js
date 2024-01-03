1bbc11c0268189b34c32892c6edc6bd0a56487ff
Rizal Nasrulloh <rizalxrpl2@gmail.com>
2024-01-03 22:29:08 +0800

    trask 9 - consume api

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,13 +1,23 @@
 import Layout from '@/layout'
 import { useRouter } from "next/router"
+import { useEffect, useState } from 'react';
   
 export default function UserDetail() {
+  const [name, setName] = useState('')
   const router = useRouter();
   const { id } = router?.query;
 
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((res) => { setName(res.name) })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
   <Layout>
-    <p>Users by Name {id}</p>
+    <p>Users by Name {name}</p>
+    <p>Users by ID {id}</p>
   </Layout>
   );
 }
