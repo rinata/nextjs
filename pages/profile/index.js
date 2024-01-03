ed1aa2d9e9070f5de19425276f6967b6679639c4
Rizal Nasrulloh <rizalxrpl2@gmail.com>
2024-01-03 22:17:39 +0800

    task 7 - routing

---
+++ pages/profile/index.js
@@ -0,0 +1,13 @@
+import Layout from '@/layout'
+
+export default function Profile() {
+  return (
+    <>
+      <Layout>
+        <h1 className="text-3xl font-bold underline">
+          Ini adalah halaman profile
+        </h1>
+      </Layout>
+    </>
+  )
+}
